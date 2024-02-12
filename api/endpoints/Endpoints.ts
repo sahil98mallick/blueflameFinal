d6252469d61aa67728e5fa6bb52885e572f5628e
sahil98mallick <sahil98mallick@gmail.com>
2024-02-12 15:16:35 +0530

    First Commit

---
+++ api/endpoints/Endpoints.ts
@@ -0,0 +1,17 @@
+export const primaryURL = "https://api.blueflame.ai/"
+
+export const endpoints = {
+    auth: {
+        login: "dev/login",
+    },
+    chats: {
+        allchats: "dev/v1/ChatThread/list",
+        singlechat: (id: String | number) => `dev/v1/ChatThread/${id}`,
+    },
+    models: {
+        allmodels: "dev/v1/Model/list"
+    },
+    profile: {
+        profileview: "dev/me",
+    }
+}
